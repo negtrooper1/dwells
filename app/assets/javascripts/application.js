405110b53f85e4847c1b15cc782b2f85bb73cd04
Devon Wells <devon@dwells.co>
2016-03-24 14:27:48 -0400

    messing around with jquery, but no real changes other than removing the tag on index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require turbolinks
+//= require phaser
 //= require_tree .
 //= require jquery.colorbox
-//= require phaser
